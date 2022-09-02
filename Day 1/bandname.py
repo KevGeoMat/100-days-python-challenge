4affad188a10996e99d17d6025f7460d4d80ed32
KevGeoMat <99088295+KevGeoMat@users.noreply.github.com>
2022-09-02 10:36:39 +0530

    Day 1

---
+++ Day 1/bandname.py
@@ -0,0 +1,13 @@
+#a print function is used to print text onto the console
+# syntax : print("enter the text")
+print("Welcome to the band name generator")
+
+#variables are used to store data
+# syntax : variablename = value
+
+#inputs are used to take input from the user
+#syntax : input("enter the prompt")
+city = input("Enter the name of the city in which you grew up: ")
+pet = input("Enter your pet's name: ")
+
+print("Your band name could be " + city + ' ' + pet)
